9b7d5261dc74ff88025a4a77889411f454514ee6
David Gengenbach <info@davidgengenbach.de>
2019-12-07 22:15:39 +0100

    Using system-wide systemd instead of user

--- install_systemd_service.sh
+++ install_systemd_service.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CONFIG_DIR=~/.config/systemd/user
+CONFIG_DIR=/etc/systemd/system
 TARGET=$CONFIG_DIR/doorbell_pi.service
 
 echo -e "This installs a systemd service into\n\n\t${TARGET}\n\n"
@@ -8,6 +8,6 @@ echo -e "This installs a systemd service into\n\n\t${TARGET}\n\n"
 # Overwrite WORKING_DIR with the current directory
 awk "{gsub(/WORKING_DIR/,\"$(pwd -P)/doorbell-pi\")}1" doorbell_pi.service > $TARGET
 
-systemctl --user daemon-reload
-systemctl --user restart doorbell_pi
-systemctl --user enable doorbell_pi
+systemctl daemon-reload
+systemctl restart doorbell_pi
+systemctl enable doorbell_pi
